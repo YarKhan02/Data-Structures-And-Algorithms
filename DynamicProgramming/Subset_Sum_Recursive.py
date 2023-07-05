19a41828d97c65bea905682ff33ff5fb14a5af6b
Yar Khan <72460103+YarKhan02@users.noreply.github.com>
2023-07-06 00:20:48 +0500

    Add files via upload

--- DynamicProgramming/Subset_Sum_Recursive.py
+++ DynamicProgramming/Subset_Sum_Recursive.py
@@ -1,19 +1,27 @@
 def subset_sum(arr, target):
-    y = len(arr)
+    y = len(arr) - 1
+    i = 0
     
     def backtrack(n, total):
         if total == target:
             return True
 
         if n == -1:
-            return False
+            nonlocal i
+            i += 1
+            if i > y:
+                return False
+            return backtrack(len(arr) - i, 0)
 
         if total <= target:
             return backtrack(n - 1, total + arr[n])
         elif total > target:
-            total -= arr[n]
+            if n == y and i == 0:
+                total -= arr[n]
+            else:
+                total -= arr[n + 1]
             return backtrack(n - 1, total)
 
-    return backtrack(y - 1, 0)
+    return backtrack(y, 0)
 
-print(subset_sum([2, 3, 7, 8, 10], 30))
+print(subset_sum([2, 3, 7, 8, 10], 19))
