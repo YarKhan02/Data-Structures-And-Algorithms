094f1c6b320d8bda0caf73ff093c2ff5b8ff2302
Yar Khan <72460103+YarKhan02@users.noreply.github.com>
2023-07-16 03:42:28 +0500

    Add files via upload

--- DynamicProgramming/Coin_Change_I.py
+++ DynamicProgramming/Coin_Change_I.py
@@ -1,20 +1,41 @@
 import random
 
-def coin_change(coins, n):
+def coin_change(coins, amount):
     r = len(coins)
-    t = [[0 if i == 0 and j != 0 else 1 if i != 0 and j == 0 else 0 for j in range(n + 1)] for i in range(r + 1)]
+    t = [[0 if i == 0 and j != 0 else 1 if i != 0 and j == 0 else -1 for j in range(amount + 1)] for i in range(r + 1)]
 
     for i in range(1, r + 1):
-        for j in range(1, n + 1):
+        for j in range(1, amount + 1):
             if coins[i - 1] <= j:
                 t[i][j] = t[i][j - coins[i - 1]] + t[i - 1][j]
             else:
                 t[i][j] = t[i - 1][j]
 
-    return t[r][n]
+    return t[r][amount]
 
 
-coins = [1, 2, 3]
-n = 5
+coins = [1, 2, 5]
+n = 3
+
+print(coin_change(coins, n))
+
+
+def coin_change(coins, amount):
+    num_coins = len(coins)
+    dp = [float('inf')] * (amount + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, amount + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    if dp[amount] == float('inf'):
+        return -1  # If it's not possible to make the amount with given coins
+
+    return dp[amount]
+
+
+coins = [1, 2, 5]
+n = 11
 
 print(coin_change(coins, n))
